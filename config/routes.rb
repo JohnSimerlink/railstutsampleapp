c6d0769d6bbeac6c943a3b74b65b72e3d6863324
JohnSimerlink <johnsimerlink@gmail.com>
2018-04-19 21:14:37 -0700

    Edit guard file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
-Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-
+Rails.application.routes.draw do 
+  get 'static_pages/about'
   get 'static_pages/home'
-
   get 'static_pages/help'
 
-  get 'static_pages/about'
+  get 'foo/bar'
+  get 'foo/baz'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
 end
